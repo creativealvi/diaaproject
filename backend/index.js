9c7882d173f08292997f055f4433ce64c3e710db
creativealvi <techalvibd@gmail.com>
2025-04-15 02:54:20 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { SessionsClient } from '@google-cloud/dialogflow-cx';
-import fs from 'fs';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
